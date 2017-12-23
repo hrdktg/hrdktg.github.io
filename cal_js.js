5c387c65c7ae1790d7e043aaa3af3beb1d9cca07
hardik tyagi <hrdktg@gmail.com>
2017-12-24 02:20:07 +0530

    Added basic functionality to generate calendar

--- cal_js.js
+++ cal_js.js
@@ -1,20 +1,24 @@
-function getCal() {
-    var fDate=new Date();
+
+
+function getCal(m,yr) {
+    var fDate=new Date(m+' 1 , '+yr);
     
     var cDate=fDate.getDate();
     var cMonth=fDate.getMonth();
     var cYear=fDate.getFullYear();
     var febDays=28;
     
-    if (cMonth==1) { //For february Check leap yr
+    if (cMonth==1) { 
+      //For february Check for leap year
       if( (cYear%4==0 && cYear%100!=0) || cYear%400==0)
         febDays=29;
     }
       
     var divCal=document.getElementById("cal");
+    var divCal2=document.getElementById("cal2");
     divCal.innerHTML=cDate+"/"+cMonth+" "+febDays;
+    divCal2.innerHTML=cDate+"/"+cMonth+" "+febDays;
     
-    var divDeb=document.getElementById("deb");
     
     var monthn = ["January","February","March","April","May","June","July","August","September","October","November", "December"];
     
@@ -25,12 +29,13 @@ function getCal() {
     var fDate=new Date(cMonth+1+' 1 , '+cYear);
     var monthStarts=fDate.getDay();
     divCal.innerHTML=dayn[monthStarts];
+    divCal.innerHTML=dayn[monthStarts];
     
     var calTable="\t<tr>\n";
-    //Insert Day Name at the top
+    //Insert Day Names at the top
     var i=0;
     while(i<7) {
-      calTable+="<td>"+dayn[i]+"</td>"
+      calTable+="<td class='dayNames'>"+dayn[i]+"</td>"
       i++;      
     }
     calTable+="</tr><tr>"
@@ -42,8 +47,6 @@ function getCal() {
       wd++;
       calTable+="\t\t<td class='blankSpace'> </td>\n";
     }
-  
-    divDeb.innerHTML=DaysInMonth[cMonth]+" "+i;
     
     //Insert Dates now
     while(i<DaysInMonth[cMonth]) {
@@ -61,4 +64,10 @@ function getCal() {
     calBody+="</table";
       
     divCal.innerHTML=calBody;
+    divCal2.innerHTML=calBody;
+
+    var CalHead=document.getElementById("calHead");
+    var CalHead2=document.getElementById("calHead2");
+    calHead.innerHTML="<h2>"+monthn[cMonth]+"</h2>";
+    calHead2.innerHTML="<h2>"+monthn[cMonth]+"</h2>";
   }
