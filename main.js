aaf4ff0fffa18586ac0dfcf3e615e166a99118ec
hrdktg <hrdktg@gmail.com>
2019-03-08 01:38:00 +0530

    Adding remote nodejs server for leave anonymous message app

---
+++ main.js
@@ -0,0 +1,97 @@
+form = document.getElementById("form");
+loader = document.getElementById("loader");
+cont = document.getElementById("main-content");
+msgList = document.getElementById("msgList");
+
+
+const api_url = 'https://shoujiki.herokuapp.com/send';
+const get_api_url = 'https://shoujiki.herokuapp.com/getMsg';
+
+listAllMsg();
+loader.style.display='none';
+var ctMsg=0;
+
+form.addEventListener('submit', (event)=> {
+    event.preventDefault();
+    console.log('Form was submitted');
+
+    const formData = new FormData(form);
+
+    //Get name and msg from form
+    const name = formData.get('name');
+    const content = formData.get('msg');
+
+    console.log([name,content]);
+
+    const mdata = {
+        name,
+        content
+    };
+
+    //Send mdata to nodejs app as json data
+    fetch(api_url, {
+        method: 'POST',
+        body: JSON.stringify(mdata),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }).then(res=>res.json())
+      .then(createdData => {
+          console.log(createdData, "recd on client");
+
+          ctMsg++;
+          if(ctMsg>=3) {
+              ctMsg=0;
+              hideForm(); 
+          }
+
+          console.log(ctMsg, " current number of msg");
+          listAllMsg();
+      })
+      .catch(err=>{
+          console.log(err, "wtfff");
+      });
+});
+
+function showForm() {
+    loader.style.display='none';
+    cont.style.display="block";
+}
+
+function hideForm() {
+    loader.style.display='block';
+    cont.style.display="none";
+    setTimeout(showForm, 10000);
+}
+
+function listAllMsg() {
+    msgList.innerHTML = "";
+    fetch(get_api_url)
+    .then(response => response.json())
+    .then(mews => {
+        console.log(mews);
+        form.reset();
+        mews.forEach(mew => {
+            const div = document.createElement('div');
+            div.classList.add("tile", "is-child", "notification", "is-primary", "box");
+            
+            const mdate = document.createElement('p');
+            mdate.classList.add("subtitle", "is-4");
+            mdate.textContent = mew.created;
+
+            const header = document.createElement('h3');
+            header.classList.add("title", "is-3");
+            header.textContent = mew.name;
+
+            const contents = document.createElement('p');
+            contents.classList.add("subtitle", "is-3");
+            contents.textContent = mew.content;
+
+            div.appendChild(mdate);
+            div.appendChild(header);
+            div.appendChild(contents);
+
+            msgList.appendChild(div);
+        });
+    });
+}
